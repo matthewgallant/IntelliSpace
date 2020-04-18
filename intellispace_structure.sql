45b2caf50e99b30047b33edd23a9203f7b41c8f9
Matt Gallant <matt@matthewgallant.me>
2020-04-18 11:54:38 -0400

    Added a Home Redirect

--- intellispace_structure.sql
+++ intellispace_structure.sql
@@ -7,7 +7,7 @@
 #
 # Host: localhost (MySQL 5.7.26)
 # Database: intellispace_mbsic
-# Generation Time: 2020-02-19 23:09:34 +0000
+# Generation Time: 2020-04-18 15:46:17 +0000
 # ************************************************************
 
 
@@ -23,8 +23,6 @@
 # Dump of table classes
 # ------------------------------------------------------------
 
-DROP TABLE IF EXISTS `classes`;
-
 CREATE TABLE `classes` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `teacher` varchar(512) DEFAULT NULL,
@@ -33,23 +31,25 @@ CREATE TABLE `classes` (
   `description` text,
   `time` varchar(64) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-LOCK TABLES `classes` WRITE;
-/*!40000 ALTER TABLE `classes` DISABLE KEYS */;
 
-INSERT INTO `classes` (`id`, `teacher`, `class`, `period`, `description`, `time`)
-VALUES
-	(9,'Mr. Roberts','Physics','Period 4','Hollywood physics projects','07:42 AM');
 
-/*!40000 ALTER TABLE `classes` ENABLE KEYS */;
-UNLOCK TABLES;
+# Dump of table logins
+# ------------------------------------------------------------
+
+CREATE TABLE `logins` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `email` varchar(512) DEFAULT NULL,
+  `passwd` varchar(512) DEFAULT NULL,
+  `name` varchar(256) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 
 
 # Dump of table setup
 # ------------------------------------------------------------
-
-DROP TABLE IF EXISTS `setup`;
 
 CREATE TABLE `setup` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
@@ -58,23 +58,12 @@ CREATE TABLE `setup` (
   `system_pages` varchar(512) DEFAULT NULL,
   `system_version` varchar(8) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-LOCK TABLES `setup` WRITE;
-/*!40000 ALTER TABLE `setup` DISABLE KEYS */;
-
-INSERT INTO `setup` (`id`, `system_name`, `system_theme`, `system_pages`, `system_version`)
-VALUES
-	(1,'Mt. Blue Success & Innovation Center','dark','students, classes, tools','2.0');
-
-/*!40000 ALTER TABLE `setup` ENABLE KEYS */;
-UNLOCK TABLES;
 
 
 # Dump of table students
 # ------------------------------------------------------------
-
-DROP TABLE IF EXISTS `students`;
 
 CREATE TABLE `students` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
@@ -85,27 +74,12 @@ CREATE TABLE `students` (
   `period` varchar(32) DEFAULT NULL,
   `time` varchar(64) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-LOCK TABLES `students` WRITE;
-/*!40000 ALTER TABLE `students` DISABLE KEYS */;
-
-INSERT INTO `students` (`id`, `name`, `grade`, `class`, `teacher`, `period`, `time`)
-VALUES
-	(21,'Matthew Gallant','Senior','Physics','Mr. Roberts','Period 5','12/29/2019 20:20 PM'),
-	(22,'Matt Gallant','Senior','English','Mr Roberto','Period 3','12/29/2019 20:24 PM'),
-	(23,'Jacob Mealey','Junior','Algebra II','Ms. Arnold','Period 3','01/03/2020 17:45 PM'),
-	(24,'Test','Freshman','Tes','Tes','Period 1','01/03/2020 17:46 PM'),
-	(25,'Johnny Boy','Sophomore','Yup','Mr. Taylor','Period 2','01/17/2020 10:32 AM');
-
-/*!40000 ALTER TABLE `students` ENABLE KEYS */;
-UNLOCK TABLES;
 
 
 # Dump of table toolout
 # ------------------------------------------------------------
-
-DROP TABLE IF EXISTS `toolout`;
 
 CREATE TABLE `toolout` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
@@ -113,27 +87,12 @@ CREATE TABLE `toolout` (
   `asset` int(6) DEFAULT NULL,
   `time` varchar(64) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=34 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-LOCK TABLES `toolout` WRITE;
-/*!40000 ALTER TABLE `toolout` DISABLE KEYS */;
-
-INSERT INTO `toolout` (`id`, `name`, `asset`, `time`)
-VALUES
-	(29,'Matthew Gallant',111222,'12/27/2019 10:27 AM'),
-	(30,'Jacob Mealey',333444,'12/27/2019 10:28 AM'),
-	(31,'Matthew Gallant',111222,'12/29/2019 20:22 PM'),
-	(32,'Matt Gallant',111222,'12/29/2019 20:23 PM'),
-	(33,'Matt G',111222,'01/03/2020 18:43 PM');
-
-/*!40000 ALTER TABLE `toolout` ENABLE KEYS */;
-UNLOCK TABLES;
 
 
 # Dump of table tools
 # ------------------------------------------------------------
-
-DROP TABLE IF EXISTS `tools`;
 
 CREATE TABLE `tools` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
@@ -141,18 +100,8 @@ CREATE TABLE `tools` (
   `asset` int(6) DEFAULT NULL,
   `possesion` varchar(256) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-LOCK TABLES `tools` WRITE;
-/*!40000 ALTER TABLE `tools` DISABLE KEYS */;
-
-INSERT INTO `tools` (`id`, `name`, `asset`, `possesion`)
-VALUES
-	(1,'Dremel Kit',111222,'Matt G'),
-	(13,'Raspberry Pi 4',333444,NULL);
-
-/*!40000 ALTER TABLE `tools` ENABLE KEYS */;
-UNLOCK TABLES;
 
 
 
